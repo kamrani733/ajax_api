a9daf8734fb94a9c87761ac62fe9228e33fea040
fateme kamrani <fatemekamrani733@gmail.com>
2022-10-11 00:32:14 +0330

    recode to make size smaller
    recode to make size smaller

--- assets/js/index.js
+++ assets/js/index.js
@@ -62,151 +62,80 @@ $(document).ready(function () {
               </ul>
             </td>
           </tr>
-     
         `;
     },
   });
-  $(".selectPsd").click(function () {
-    $.ajax({
-      type: "POST",
-      url: "https://541.ir/fronttest/fronttest.php",
-      data: "param=psd",
-      dataType: "json",
-      success: function (data) {
-        const showresults = document.getElementById("showresults");
-        const countItems = Object.keys(data.items);
-        const sufix = document.getElementById("sufix-file");
-        sufix.innerHTML = "";
-        for (let i = 0; i < countItems.length; i++) {
-          console.log(data.items[`${countItems[i]}`]);
-          sufix.innerHTML += `
-          <img class="showDetals" onclick="showDetailMini(${
-            data.items[`${countItems[i]}`]
-          })" src="./assets/image/wordDetail2.png" alt="" /> `;
-        }
-        showresults.innerHTML = `
-        <table>
-          <tr class="table-head">
-            <th colspan="5">${data.item_data.title}</th>
-          </tr>
-          <tr>
-            <td class="table-col1">ناشر</td>
-            <td class="table-col2">${data.item_data.publisher}</td>
-            <td class="table-col1">قابلیت ویرایش</td>
-            <td class="table-col2">${data.item_data.edit}</td>
-          </tr>
-          <tr>
-            <td class="table-col1">پسوند فایل</td>
-            <td class="table-col2">${data.item_data.extension}</td>
-            <td class="table-col1">قابلیت پرینت</td>
-            <td class="table-col2">${data.item_data.print}</td>
-          </tr>
-          <tr>
-            <td class="table-col1">نرم افزار ویرایش</td>
-            <td class="table-col2">${data.item_data.editor}</td>
-            <td class="table-col1">حجم فایل</td>
-            <td class="table-col2">${data.item_data.size}</td>
-          </tr>
-          <tr>
-            <td class="table-col1">دسته بندی</td>
-            <td class="table-col2" colspan="3">
-              ${data.item_data.categories}
-            </td>
-          </tr>
-          <tr>
-            <td class="table-col1">مقاله آموزشی</td>
-            <td class="table-col2" colspan="3">
-              ${data.item_data.post}
-            </td>
-          </tr>
-
-          <tr>
-            <td class="table-col1">توضیحات</td>
-            <td class="table-col3" colspan="3">
-               <ul>
-                <li>
-                  ${data.item_data.description}
-                </li>
-              </ul>
-            </td>
-          </tr>
-        </table>
-        `;
-      },
-    });
-  });
-  $(".selectWord").click(function () {
-    $.ajax({
-      type: "POST",
-      url: "https://541.ir/fronttest/fronttest.php",
-      data: "param=docx",
-      dataType: "json",
-
-      success: function (data) {
-        const showresults = document.getElementById("showresults");
-        const countItems = Object.keys(data.items);
-        const sufix = document.getElementById("sufix-file");
-        sufix.innerHTML = "";
-        for (let i = 0; i < countItems.length; i++) {
-          console.log(data.items[`${countItems[i]}`]);
-
-          sufix.innerHTML += `
-          <img class="showDetals" onclick="showDetailMini(${
-            data.items[`${countItems[i]}`]
-          })" src="./assets/image/wordDetail2.png" alt="" /> `;
-        }
-        showresults.innerHTML = `
-        <table>
-          <tr class="table-head">
-            <th colspan="5">${data.item_data.title}</th>
-          </tr>
-          <tr>
-            <td class="table-col1">ناشر</td>
-            <td class="table-col2">${data.item_data.publisher}</td>
-            <td class="table-col1">قابلیت ویرایش</td>
-            <td class="table-col2">${data.item_data.edit}</td>
-          </tr>
-          <tr>
-            <td class="table-col1">پسوند فایل</td>
-            <td class="table-col2">${data.item_data.extension}</td>
-            <td class="table-col1">قابلیت پرینت</td>
-            <td class="table-col2">${data.item_data.print}</td>
-          </tr>
-          <tr>
-            <td class="table-col1">نرم افزار ویرایش</td>
-            <td class="table-col2">${data.item_data.editor}</td>
-            <td class="table-col1">حجم فایل</td>
-            <td class="table-col2">${data.item_data.size}</td>
-          </tr>
-          <tr>
-            <td class="table-col1">دسته بندی</td>
-            <td class="table-col2" colspan="3">
-              ${data.item_data.categories}
-            </td>
-          </tr>
-          <tr>
-            <td class="table-col1">مقاله آموزشی</td>
-            <td class="table-col2" colspan="3">
-              ${data.item_data.post}
-            </td>
-          </tr>
-
-          <tr>
-            <td class="table-col1">توضیحات</td>
-            <td class="table-col3" colspan="3">
-               <ul>
-                <li>
-                  ${data.item_data.description}
-                </li>
-              </ul>
-            </td>
-          </tr>
-        </table>
-        `;
-      },
-    });
-  });
 });
+function selectedType(id) {
+  console.log(id);
+  $.ajax({
+    type: "POST",
+    url: "https://541.ir/fronttest/fronttest.php",
+    data: `param=${id}`,
+    dataType: "json",
+    success: function (data) {
+      const showresults = document.getElementById("showresults");
+      const countItems = Object.keys(data.items);
+      const sufix = document.getElementById("sufix-file");
+      sufix.innerHTML = "";
+      for (let i = 0; i < countItems.length; i++) {
+        console.log(data.items[`${countItems[i]}`]);
+        sufix.innerHTML += `
+          <img class="showDetals" onclick="showDetailMini(${
+            data.items[`${countItems[i]}`]
+          })" src="./assets/image/wordDetail2.png" alt="" /> `;
+      }
+      showresults.innerHTML = `
+      <table>
+        <tr class="table-head">
+          <th colspan="5">${data.item_data.title}</th>
+        </tr>
+        <tr>
+          <td class="table-col1">ناشر</td>
+          <td class="table-col2">${data.item_data.publisher}</td>
+          <td class="table-col1">قابلیت ویرایش</td>
+          <td class="table-col2">${data.item_data.edit}</td>
+        </tr>
+        <tr>
+          <td class="table-col1">پسوند فایل</td>
+          <td class="table-col2">${data.item_data.extension}</td>
+          <td class="table-col1">قابلیت پرینت</td>
+          <td class="table-col2">${data.item_data.print}</td>
+        </tr>
+        <tr>
+          <td class="table-col1">نرم افزار ویرایش</td>
+          <td class="table-col2">${data.item_data.editor}</td>
+          <td class="table-col1">حجم فایل</td>
+          <td class="table-col2">${data.item_data.size}</td>
+        </tr>
+        <tr>
+          <td class="table-col1">دسته بندی</td>
+          <td class="table-col2" colspan="3">
+            ${data.item_data.categories}
+          </td>
+        </tr>
+        <tr>
+          <td class="table-col1">مقاله آموزشی</td>
+          <td class="table-col2" colspan="3">
+            ${data.item_data.post}
+          </td>
+        </tr>
+
+        <tr>
+          <td class="table-col1">توضیحات</td>
+          <td class="table-col3" colspan="3">
+             <ul>
+              <li>
+                ${data.item_data.description}
+              </li>
+            </ul>
+          </td>
+        </tr>
+      </table>
+      `;
+    },
+  });
+}
 function showDetailMini(id) {
   console.log(id);
   $.ajax({
