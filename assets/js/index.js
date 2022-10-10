7a935e353d1012793eb3c43a44047cfba8983aa3
fateme kamrani <fatemekamrani733@gmail.com>
2022-10-10 16:16:24 +0330

    select file by id

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,19 +7,16 @@ $(document).ready(function () {
     success: function (data) {
       const showresults = document.getElementById("showresults");
       const sufix = document.getElementById("sufix-file");
-      console.log(Object.keys(data.items));
-      sufix.innerHTML = `
-        <div class="selected-file">
-        <img src="./assets/image/wordDetail.png" alt="" />
-      </div>
-      <div class="unSelected-file">
-        <img id="${data.items["فایل 1"]}" src="./assets/image/wordDetail2.png" alt="" />
-        <img src="./assets/image/wordDetail3.png" alt="" />
-        <img class="loadmore" src="./assets/image/wordDetail3.png" alt="" />
-      </div>
-        `;
+      const countItems = Object.keys(data.items);
+      console.log(countItems);
+      for (let i = 0; i < countItems.length; i++) {
+        sufix.innerHTML = `
+          <img class="showDetals" onclick="showDetailMini(${data.items["فایل 1"]})" src="./assets/image/wordDetail2.png" alt="" />
+          <img class="showDetals" onclick="showDetailMini(${data.items["فایل 2"]})" src="./assets/image/wordDetail2.png" alt="" />
+          <img class="loadMore" src="./assets/image/wordDetail2.png" alt="" />`;
+      }
+
       showresults.innerHTML = `
-        
           <tr class="table-head">
             <th colspan="5">${data.title}</th>
           </tr>
@@ -76,6 +73,15 @@ $(document).ready(function () {
       dataType: "json",
       success: function (data) {
         const showresults = document.getElementById("showresults");
+        const sufix = document.getElementById("sufix-file");
+        const countItems = Object.keys(data.items);
+        console.log(countItems);
+        for (let i = 0; i < countItems.length; i++) {
+          sufix.innerHTML = `
+            <img class="showDetals" onclick="showDetailMini(${data.items["فایل 3"]})" src="./assets/image/wordDetail2.png" alt="" />
+            <img class="showDetals" onclick="showDetailMini(${data.items["فایل 4"]})" src="./assets/image/wordDetail2.png" alt="" />
+            <img class="loadMore" src="./assets/image/wordDetail2.png" alt="" />`;
+        }
         showresults.innerHTML = `
         <table>
           <tr class="table-head">
@@ -134,6 +140,17 @@ $(document).ready(function () {
       data: "param=docx",
       dataType: "json",
       success: function (data) {
+        console.log(data);
+        console.log(data.items);
+        const sufix = document.getElementById("sufix-file");
+        const countItems = Object.keys(data.items);
+        console.log(countItems);
+        for (let i = 0; i < countItems.length; i++) {
+          sufix.innerHTML = `
+            <img class="showDetals" onclick="showDetailMini(${data.items["فایل 1"]})" src="./assets/image/wordDetail2.png" alt="" />
+            <img class="showDetals" onclick="showDetailMini(${data.items["فایل 2"]})" src="./assets/image/wordDetail2.png" alt="" />
+            <img class="loadMore" src="./assets/image/wordDetail2.png" alt="" />`;
+        }
         const showresults = document.getElementById("showresults");
         showresults.innerHTML = `
         <table>
@@ -187,3 +204,63 @@ $(document).ready(function () {
     });
   });
 });
+function showDetailMini(id) {
+  console.log(id);
+  $.ajax({
+    type: "POST",
+    url: "https://541.ir/fronttest/fronttest.php",
+    data: `param=${id}`,
+    dataType: "json",
+    success: function (data) {
+      const showresults = document.getElementById("showresults");
+      showresults.innerHTML = `
+      <table>
+        <tr class="table-head">
+          <th colspan="5">${data.title}</th>
+        </tr>
+        <tr>
+          <td class="table-col1">ناشر</td>
+          <td class="table-col2">${data.publisher}</td>
+          <td class="table-col1">قابلیت ویرایش</td>
+          <td class="table-col2">${data.edit}</td>
+        </tr>
+        <tr>
+          <td class="table-col1">پسوند فایل</td>
+          <td class="table-col2">${data.extension}</td>
+          <td class="table-col1">قابلیت پرینت</td>
+          <td class="table-col2">${data.print}</td>
+        </tr>
+        <tr>
+          <td class="table-col1">نرم افزار ویرایش</td>
+          <td class="table-col2">${data.editor}</td>
+          <td class="table-col1">حجم فایل</td>
+          <td class="table-col2">${data.size}</td>
+        </tr>
+        <tr>
+          <td class="table-col1">دسته بندی</td>
+          <td class="table-col2" colspan="3">
+            ${data.categories}
+          </td>
+        </tr>
+        <tr>
+          <td class="table-col1">مقاله آموزشی</td>
+          <td class="table-col2" colspan="3">
+            ${data.post}
+          </td>
+        </tr>
+
+        <tr>
+          <td class="table-col1">توضیحات</td>
+          <td class="table-col3" colspan="3">
+             <ul>
+              <li>
+                ${data.description}
+              </li>
+            </ul>
+          </td>
+        </tr>
+      </table>
+      `;
+    },
+  });
+}
